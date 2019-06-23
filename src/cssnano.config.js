53ec1e1135e9a935a05c339b75ee32a9ab55e325
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2019-06-23 14:41:46 -0700

    test 8

--- src/cssnano.config.js
+++
@@ -1,9 +0,0 @@
-const defaultPreset = require("cssnano-preset-default");
-
-module.exports = defaultPreset({
-  normalizeWhitespace: false,
-  reduceTransforms: false,
-  zindex: false,
-  orderedValues: false,
-  calc: false
-});
